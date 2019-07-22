25b7eac224deccaa6a54e8d056a433c265cf5df3
ahmedalhulaibi <ahmed.alhulaibi41@gmail.com>
2019-07-21 23:50:13 -0400

    Add faas to schedule maas container

---
+++ maas-faas/go.mod
@@ -0,0 +1,17 @@
+module github.com/ahmedalhulaibi/maas/maas-faas
+
+go 1.12
+
+require (
+	github.com/Microsoft/go-winio v0.4.13 // indirect
+	github.com/docker/distribution v2.7.1+incompatible // indirect
+	github.com/docker/docker v1.4.2-0.20190710153559-aa8249ae1b8b
+	github.com/docker/go-connections v0.4.0 // indirect
+	github.com/docker/go-units v0.4.0 // indirect
+	github.com/gogo/protobuf v1.2.1 // indirect
+	github.com/opencontainers/go-digest v1.0.0-rc1 // indirect
+	github.com/opencontainers/image-spec v1.0.1 // indirect
+	github.com/stretchr/testify v1.3.0 // indirect
+	golang.org/x/net v0.0.0-20190628185345-da137c7871d7 // indirect
+	google.golang.org/grpc v1.22.0 // indirect
+)
---
+++ maas-faas/handler.go
@@ -0,0 +1,83 @@
+package function
+
+import (
+	"context"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
+
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/client"
+)
+
+func Handle(w http.ResponseWriter, r *http.Request) {
+	gitURL := r.URL.Query().Get("giturl")
+
+	if gitURL == "" {
+		handleErr(http.StatusBadRequest, "Missing 'giturl' query parameter", w)
+		return
+	}
+
+	ctx := context.Background()
+	cli, err := client.NewEnvClient()
+	if err != nil {
+		handleErr(http.StatusInternalServerError, err.Error(), w)
+		return
+	}
+
+	reader, err := cli.ImagePull(ctx, "ahmedalhulaibi/maas:latest", types.ImagePullOptions{})
+	if err != nil {
+		handleErr(http.StatusInternalServerError, err.Error(), w)
+		return
+	}
+	io.Copy(os.Stdout, reader)
+
+	resp, err := cli.ContainerCreate(ctx, &container.Config{
+		Image:      "ahmedalhulaibi/maas:latest",
+		Entrypoint: []string{"maas.sh", gitURL},
+		Tty:        true,
+		Volumes:    map[string]struct{}{"/var/run/docker.sock:/var/run/docker.sock": {}},
+	}, nil, nil, "")
+
+	if err != nil {
+		handleErr(http.StatusInternalServerError, err.Error(), w)
+		return
+	}
+
+	if err := cli.ContainerStart(ctx, resp.ID, types.ContainerStartOptions{}); err != nil {
+		handleErr(http.StatusInternalServerError, err.Error(), w)
+		return
+	}
+
+	statusCh, errCh := cli.ContainerWait(ctx, resp.ID, container.WaitConditionNotRunning)
+	select {
+	case err := <-errCh:
+		if err != nil {
+			handleErr(http.StatusInternalServerError, err.Error(), w)
+			return
+		}
+	case <-statusCh:
+	}
+
+	out, err := cli.ContainerLogs(ctx, resp.ID, types.ContainerLogsOptions{ShowStdout: true})
+	if err != nil {
+		handleErr(http.StatusInternalServerError, err.Error(), w)
+		return
+	}
+
+	output, err := ioutil.ReadAll(out)
+	if err != nil {
+		handleErr(http.StatusInternalServerError, err.Error(), w)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Write(output)
+}
+
+func handleErr(status int, message string, w http.ResponseWriter) {
+	w.WriteHeader(status)
+	w.Write([]byte(message))
+}
